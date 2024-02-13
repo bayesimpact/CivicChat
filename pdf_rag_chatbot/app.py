76ba0542882a80f3bfd0947962c72b08dd67e446
Eric Boucher <eric.p.boucher@gmail.com>
2024-02-13 18:25:00 +0100

    cleanup

--- pdf_rag_chatbot/app.py
+++ pdf_rag_chatbot/app.py
@@ -1,4 +1,6 @@
+import logging
 import os
+import sys
 import time
 import uuid
 
@@ -15,16 +17,16 @@ from llama_index.core.memory import ChatMemoryBuffer
 from llama_index.embeddings.openai import OpenAIEmbedding
 from llama_index.llms.openai import OpenAI
 
+# Check that OPENAI_API_KEY is set
+if "OPENAI_API_KEY" not in os.environ:
+    raise ValueError("Please set the OPENAI_API_KEY environment variable.")
+
+# Define which LLM we are going to use for response generation
+# and which embedding model to create embeddings. Default is
+# GPT-3 Davinci and OpenAI embeddings model ADA
 llm = OpenAI(model="gpt-3.5-turbo", temperature=0.1)
 llama_debug = LlamaDebugHandler(print_trace_on_end=True)
-
-# Service context contains information about which LLM we
-# are going to use for response generation and which
-# embedding model to create embeddings. Default is
-# GPT-3 Davinci and OpenAI embeddings model ADA
-
-import logging
-import sys
+embed_model = OpenAIEmbedding()
 
 # Uncomment to see debug logs #
 # logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
@@ -32,11 +34,8 @@ import sys
 
 
 def load_data():
-    embed_model = OpenAIEmbedding()
-    print("embed_model loaded")
-    reader = SimpleDirectoryReader(
-        input_dir="./documents", recursive=True
-    )  # creating Callback manager object to show each sub process details \
+    reader = SimpleDirectoryReader(input_dir="./documents", recursive=True)
+    # creating Callback manager object to show each sub process details \
     callback_manager = CallbackManager([llama_debug])
     docs = reader.load_data()
     index = VectorStoreIndex.from_documents(
@@ -48,52 +47,37 @@ def load_data():
 def create_custom_chatEngine(index):
     memory = ChatMemoryBuffer.from_defaults(token_limit=3900)
     # list of `ChatMessage` objects
-    template = (
-        "Following Informations : \n"
-        "---------------------\n"
+    context_prompt = (
+        "You are a chatbot helping red cross volunteers for the 2024 Olympic Games."
+        "Here are the relevant documents for the context:\n"
         "{context_str}"
-        "\n---------------------\n"
-        "Please answer the question always in the language of the quetsion. Always stay polite and share your source."
+        "\nInstruction: Use the previous chat history, or the context above, to interact and help the user."
+        "\n Please answer the question always in the language of the quetsion. Always stay polite and share your source."
     )
-    # qa_template = PromptTemplate(template)
 
-    # query_engine = index.as_query_engine(text_qa_template=qa_template, llm=llm)
     chat_engine = index.as_chat_engine(
         chat_mode="condense_plus_context",
         memory=memory,
-        llm=llm,
-        context_prompt=(
-            "You are a chatbot helping red cross volunteers for the 2024 Olympic Games."
-            "Here are the relevant documents for the context:\n"
-            "{context_str}"
-            "\nInstruction: Use the previous chat history, or the context above, to interact and help the user."
-            "\n Please answer the question always in the language of the quetsion. Always stay polite and share your source."
-        ),
+        context_prompt=context_prompt,
         verbose=True,
     )
     return chat_engine
 
 
-def insertTable(sessionId, text):
+def insertTable(session_id, text):
     print(
         supabase.table("chatlog")
-        .upsert({"session_id": sessionId, "history": text})
+        .upsert({"session_id": session_id, "history": text})
         .execute()
     )
 
 
-# Add here your secret API-KEY from Supabase
+# Define your supabase key and url
 # For security reasons please create a enviroment variable for it
-key = os.environ["DB_KEY"] = "PASTE_YOUR_SUPABASE_DB_KEY_HERE"
+supabase_key = os.environ["DB_KEY"] = "PASTE_YOUR_SUPABASE_DB_KEY_HERE"
+supabase_url = "PASTE_YOUR_URL_HERE"
 
-# Replace with your OpenAI API key
-# For security reasons please create a enviroment variable for it
-
-# os.environ["OPENAI_API_KEY"] = "PASTE_YOUR_OPENAI_KEY_HERE"
-
-# Add here your supabase-URL
-url = "PASTE_YOUR_URL_HERE"
-
+# TODO - connect to supabase
 supabase = None
 
 # Load index from disk
@@ -108,11 +92,10 @@ except FileNotFoundError as e:
     print("data loaded")
     # Persist index
     index.storage_context.persist("./index_files")
-
 print("index is ready")
-print(index)
 
-with gr.Blocks() as demo:
+
+with gr.Blocks(fill_height=True) as demo:
     chat_engine = create_custom_chatEngine(index)
 
     # Create new session_id
@@ -146,5 +129,5 @@ with gr.Blocks() as demo:
     clear.click(lambda: None, None, chatbot, queue=True)
 
 if __name__ == "__main__":
-    print("Hello World!")
+    print("Ready to chat!")
     demo.queue().launch(share=False)
